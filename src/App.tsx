0de17171297f0a1896b31d9e2d4435e95da7e8a8
Pavel-Kliukin <9213087@gmail.com>
2024-01-27 22:08:29 +0200

    Wolt logo added to top left corner

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import DeliveryCalculator from './DeliveryCalculator';
 
 function App() {
   return (
-    <div className="bg-sky-400 w-screen h-screen flex flex-col justify-center items-center">
-      <DeliveryCalculator />
+    <div className="bg-sky-400 w-screen h-screen flex flex-col justify-start items-center">
+      <div className='w-full flex justify-start'>
+        <div className='ml-10 mt-10 w-40'>
+          <img src="./assets/wolt.png" alt="wolt logo" />
+        </div>
+      </div>
+      <div className='flex-1 flex justify-center items-center'>
+        <DeliveryCalculator />
+      </div>
     </div>
   );
 }
